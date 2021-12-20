8900dd134de7159874000b8c6d0bd6e3e7feff10
Ariel Ril <arielril@gmail.com>
2021-12-20 07:52:31 -0300

    add some tools

--- install.sh
+++ install.sh
@@ -10,13 +10,12 @@ cd /tmp/
 sudo wget https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/hacker/.profile
-echo 'export PATH=$PATH:/home/hacker/go/bin' >> /home/hacker/.profile
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.profile
-echo 'export PATH=$PATH:/home/hacker/go/bin' >> /root/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >>/home/hacker/.profile
+echo 'export PATH=$PATH:/home/hacker/go/bin' >>/home/hacker/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >>/root/.profile
+echo 'export PATH=$PATH:/home/hacker/go/bin' >>/root/.profile
 sudo rm -rf go1.17*
 cd -
-
 
 echo '[+] Installing ProjectDiscovery tools'
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
@@ -40,7 +39,7 @@ sudo chmod -R 755 /opt/seclists/
 
 echo '[+] Installing Fish Shell'
 sudo apt-get install fish -y
-sudo chsh -s `which fish`
+sudo chsh -s $(which fish)
 
 sudo curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
@@ -48,14 +47,17 @@ fisher install laughedelic/pisces
 fisher install rafaelrinaldi/pure
 
 echo '[+] Setup fish'
-echo 'set -g fish_user_paths "/home/hacker/go/bin" $fish_user_paths' >> /home/hacker/.config/fish/config.fish
-sudo echo 'set -g fish_user_paths "/home/hacker/go/bin" $fish_user_paths' >> /root/.config/fish/config.fish
+echo 'set -g fish_user_paths "/home/hacker/go/bin" $fish_user_paths' >>/home/hacker/.config/fish/config.fish
+sudo echo 'set -g fish_user_paths "/home/hacker/go/bin" $fish_user_paths' >>/root/.config/fish/config.fish
 
 echo '[+] Update and Install vim plugins'
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+echo '[+] Installing XSS-Strike'
+sudo git clone --depth 1 https://github.com/s0md3v/XSStrike.git /opt/xssstrike
+sudo chmod 755 /opt/xssstrike
+
 echo '[+] Installing extra tools'
 sudo apt-get install -y zip ripgrep build-essential bat nmap python3-pip
-
-
+go install github.com/tomnomnom/waybackurls@latest
